3ac5c27fb787207b8e2b3cca4bf2deb988b5b123
Jackson Drew <jacksonrdrew@gmail.com>
2026-01-27 12:44:58 -0800

    Added some basic Django templates

--- civic_innovation_through_you/settings.py
+++ civic_innovation_through_you/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+PROJECT_DIR = os.path.join(BASE_DIR, 'civic_innovation_through_you')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'civic_innovation_through_you.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(PROJECT_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
