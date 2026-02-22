4b9596143e3bcb70f812ce8061221b79c73f0954
Jackson Drew <jacksonrdrew@gmail.com>
2026-02-22 11:50:20 -0800

    Create City Admin Permissions and Group

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class AdminAccess(models.Model):
+    class Meta:
+        managed = False  # no table needed
+        default_permissions = ()
+        permissions = [
+            ("can_admin_site", "Can access city admin pages"),
+        ]
