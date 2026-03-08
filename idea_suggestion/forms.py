7caaf57c3773a196418153ca4624b5ce261fd625
shellshells <sheldon.smith.tech@gmail.com>
2026-03-07 18:44:31 -0800

    Updated sidebar implementation to be more modular. Added sidebar to city admin idea inbox with search, sort, and filter functionality. Removed sort from main inbox and updated to allow ascending and descending options

--- idea_suggestion/forms.py
+++ idea_suggestion/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from . import models
+from common.choices import Topic_Options
 
 # A form for users to fillout so they can submit their ideas
 class SubmitIdeaForm(forms.ModelForm):
@@ -7,3 +8,38 @@ class SubmitIdeaForm(forms.ModelForm):
         # Use a model you created and choose which of its fields you want to explicitly use
         model = models.Idea
         fields = ['subject_line', 'location', 'topic', 'message']
+
+
+# A form used for filtering city admin inbox options
+class IdeaFilterForm(forms.Form):
+    topic_filter = forms.MultipleChoiceField(
+        choices = Topic_Options.choices,
+        required = False,
+        widget = forms.CheckboxSelectMultiple
+    )
+
+    read_filter = forms.ChoiceField(
+        choices = [("", "All"), ("True", True), ("False", False)],
+        required = False)
+    
+# A form used for sorting city admin inbox options
+class IdeaSortForm(forms.Form):
+    sort_options = forms.ChoiceField(
+        choices = [
+            ("", "Auto"),
+            ("topic", "Topic (A-Z)"),
+            ("-topic", "Topic (Z-A)"),
+            ("subject_line", "Subject Line (A-Z)"),
+            ("-subject_line", "Subject Line (Z-A)"),
+            ("time_stamp", "Date (Oldest)"),
+            ("-time_stamp", "Date (Newest)"),
+            ("read_status", "Unread"),
+            ("-read_status", "Read")
+        ],
+        required = False
+    )
+
+#A form used to search for terms in the city admin inbox
+class IdeaSearchForm(forms.Form):
+    subject_search = forms.CharField(max_length = 50, required = False)
+    content_search = forms.CharField(max_length = 50, required = False)
