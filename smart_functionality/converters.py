f14316defd1786259f34f8fb508cb339c51cb146
hat-owl <91843209+hat-owl@users.noreply.github.com>
2026-04-28 19:24:35 -0700

    Add stripping markdown to the data conversion

--- smart_functionality/converters.py
+++ smart_functionality/converters.py
@@ -1,3 +1,5 @@
+import strip_markdown
+
 '''
 Method to convert data into AI friendly strings
 Param: data, the data objects to be converted into AI friendly strings
@@ -11,15 +13,19 @@ def Convert_Data(data):
     
     # The data doesn't need processing
     elif type(data) is str:
-        return data
+        string_data = data
     
     # The data does need to be processed
     else:
-        string = ""
+        string_data = ""
         for field in data._meta.get_fields():
             value = getattr(data, field.name, None)
             value = str(value) if value is not None else ""
 
-            string += f"{field.name}: {value}. "
+            string_data += f"{field.name}: {value}. "
 
-        return string
+    # Process markdown
+
+    string_data_no_markdown = strip_markdown.strip_markdown(string_data)
+
+    return string_data_no_markdown
