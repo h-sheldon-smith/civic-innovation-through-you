1c5525b0f4cbde0ce98c4f2a34a1cec28b40edb3
hat-owl <91843209+hat-owl@users.noreply.github.com>
2026-04-28 20:41:31 -0700

    Create a reciever that triggers the smart post moderation

---
+++ idea_forum/signals.py
@@ -0,0 +1,14 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.db import transaction
+from machina.core.db.models import get_model
+
+from idea_forum.services.smart_controller import SmartController
+
+Post = get_model('forum_conversation', 'Post')
+
+@receiver(post_save, sender=Post)
+def trigger_smart_controller(sender, instance, created, **kwargs):
+    # trigger when a post is created and that post needs approval (so not moderator posts if they're set to not require approval)
+    if created and not instance.approved:
+        transaction.on_commit(SmartController.run_smart_post_moderation(instance.id))
