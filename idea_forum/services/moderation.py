2bb7437e8f1ac5e20cd03ff23ca6a546e59fc8ae
hat-owl <91843209+hat-owl@users.noreply.github.com>
2026-04-28 20:38:08 -0700

    Create post screening glue services

---
+++ idea_forum/services/moderation.py
@@ -0,0 +1,44 @@
+from django.db import transaction
+from machina.core.db.models import get_model
+
+Post = get_model('forum_conversation', 'Post')
+
+class ModerationService:
+    def get_post_text(self, post_id):
+        post = Post.objects.get(pk=post_id)
+        
+        if hasattr(post.content, 'raw'):
+            text = post.content.raw
+        else:
+            text = str(post.content)
+
+        return text
+    
+    def approve_post(self, post_id):
+        # within an atomic transaction to prevent race condition
+        with transaction.atomic():
+            post = Post.objects.select_for_update().get(pk=post_id)
+            if not post.approved: # prevent race condition
+                post.approved == True
+                post.save()
+                
+                # update the post's thread's and forum container's trackers
+                post.topic.update_trackers()
+                post.topic.forum.update_trackers()
+                
+                return True
+            
+        return False
+
+    def disapprove_post(self, post_id):
+        try:
+            post = Post.objects.get(pk=post_id)
+
+            post.delete()
+
+            post.topic.update_trackers()
+            post.topic.forum.update_trackers()
+            
+            return True
+        except Post.DoesNotExist:
+            return False
