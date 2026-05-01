5995a9867e8f46dffb6660c90fec637f16af4664
hat-owl <91843209+hat-owl@users.noreply.github.com>
2026-05-01 15:58:06 -0700

    Manually update the posts' topic and forum post counts

--- idea_forum/services/forum_tools.py
+++ idea_forum/services/forum_tools.py
@@ -19,17 +19,29 @@ class ForumInteractionService:
     def approve_post(self, post):
         if not post.approved: # prevent race condition
             print("approving post")
-
+            # with transaction.atomic():
             post.approved = True
-            post.save(update_fields=['approved'])
-            
-            # using save() breaks the post UI view's URl redirect
-            # bypassing the save method and directly doing a SQL update also breaks it
-            # Post.objects.filter(pk=post.id).update(approved=True)
+            # post.save() updates the topic/forum trackers internally
+            post.save()
             # post.refresh_from_db()
-            # post.topic.update_trackers()
-            # post.topic.forum.update_trackers()
 
+            posts_count = Post.objects.filter(topic=post.topic, approved=True).count()
+            total_posts_in_db = Post.objects.all().count()
+            posts_this_topic_any_status = Post.objects.filter(topic=post.topic).count()
+
+            print(f"Total Posts in DB: {total_posts_in_db}")
+            print(f"Total Posts in this Topic (any status): {posts_this_topic_any_status}")
+            print(f"Approved Posts in this Topic: {posts_count}")
+            # print(f"post.position(): {post.position()}")
+
+            post.topic.posts_count = posts_count
+            post.topic.save()
+
+            post.topic.forum.posts_count = posts_count
+            post.topic.forum.save()
+
+            post.save()
+            
             print(f"post.approved = {post.approved}")
             return True
             
