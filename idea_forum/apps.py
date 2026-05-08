7526a217c0283e8e092af2f2ae01777d16285731
hat-owl <91843209+hat-owl@users.noreply.github.com>
2026-05-07 20:57:49 -0700

    Comment edit

--- idea_forum/apps.py
+++ idea_forum/apps.py
@@ -11,5 +11,6 @@ class IdeaForumConfig(AppConfig):
         import idea_forum.signals
 
         if self.tranformer_pipe is None:
+            # TODO: download model to disk if not on disk, then load from disk to memory if not in memory
             self.tranformer_pipe = pipeline(model="facebook/bart-large-mnli")
             # self.tranformer_pipe("test", candidate_labels=["a"])
