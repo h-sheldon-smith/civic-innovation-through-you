568ae2cb794d5daef06a5c707aff4b34d4f61583
hat-owl <91843209+hat-owl@users.noreply.github.com>
2026-05-07 18:51:13 -0700

    Refactor: move markdown stripping out of smart_functionality's converter into idea_forum

--- idea_forum/services/screening_tools.py
+++ idea_forum/services/screening_tools.py
@@ -1,5 +1,4 @@
-# wordlist-based checker (plus leetspeek-style substitutions)
-# from better_profanity import profanity
+import strip_markdown
 
 # ML-based checker (linear SVM)
 import profanity_check
@@ -8,28 +7,28 @@ import profanity_check
 # from smart_functionality import pipeline
 # from idea_forum.services.prompts import SCREEN_POST
 
-# 3 options, currently I'm using the ML checker to screen for profanity only
 class ScreeningService:
     def screen_post_text(self, post_text):
-        profanity_pass_result = self.screen_pass_profanity(post_text)
+        profanity_check_result = self.profanity_check(post_text)
         
         # llm_pass_result = self.screen_pass_llm(self, post_text)
         
-        return profanity_pass_result
+        return profanity_check_result
     
-    def screen_pass_profanity(self, post_text):
-        # # wordlist-based checker, also handles leetspeek-style 'obfuscated profanity'
-        # wordlist_result = not profanity.contains_profanity(post_text)
-
+    def profanity_check(self, post_text):
         print(f"post_text = {post_text}")
 
-        # ML-based checker, doesn't handle leetspeek-style 'obfuscated profanity'
-        ml_result = not bool(profanity_check.predict([post_text])[0])
+        cleaned_text = strip_markdown.strip_markdown(post_text)
 
-        print(f"ml_result = {ml_result}")
+        print(f"cleaned_text = {cleaned_text}")
 
-        return ml_result
-    
+        # ML-based pofanity detection, doesn't handle leetspeek-style 'obfuscated profanity'
+        result = not bool(profanity_check.predict([cleaned_text])[0])
+
+        print(f"profanity_checker_result = {result}")
+
+        return result
+     
     # def screen_pass_llm(self, post_text):
     #     response = pipeline.Ask_AI(SCREEN_POST['task'], SCREEN_POST['format'], post_text)
     #     llm_result = (response == SCREEN_POST['true'])
