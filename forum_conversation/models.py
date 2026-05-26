e932b11bad2a30d6ab1dfb8514dbfb624fdb6bbd
hat-owl <91843209+hat-owl@users.noreply.github.com>
2026-05-26 15:55:19 -0700

    Partial work on reddit-style upvotes/downvotes

--- forum_conversation/models.py
+++ forum_conversation/models.py
@@ -4,6 +4,8 @@ from machina.apps.forum_conversation.abstract_models import AbstractPost
 
 # add the VoteModel mixin when creating the concrete Post model from the AbstracPost abstract model
 class Post(VoteModel, AbstractPost):
+    # TODO: add a num_net_votes field to the Post model (or possibly something that inherits the VoteModel mxin and plugs in to the django-vote internals)
+
     pass
 
 # Custom models should be declared above before importing django-machina's models
